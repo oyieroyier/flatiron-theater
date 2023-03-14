615a857cc57b3b6e12e8fd766da2c6e4d28ff46a
Bob Oyier <boboyier92@gmail.com>
2023-03-15 01:11:53 +0300

    Update action ✔️

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,2 +1,42 @@
 class ProductionsController < ApplicationController
+  wrap_parameters format: []
+  def index
+    render json: Production.all, status: :ok
+  end
+
+  def show
+    production = Production.find_by(id: params[:id])
+    if production
+      render json: production, status: :ok
+    else
+      render json: { error: 'Not Available' }, status: :not_found
+    end
+  end
+
+  def create
+    production = Production.create(production_params)
+    render json: production
+  end
+
+  def update
+    # Find the entry
+    production = Production.find_by(id: params[:id])
+
+    if production
+      # Update the entry
+      production.update(production_params)
+      render json: production, status: :accepted
+    else
+      render json: {
+               error: 'No such production, check Nollywood!'
+             },
+             status: :not_found
+    end
+  end
+
+  private
+
+  def production_params
+    params.permit(:title, :genre, :budget, :image, :director, :ongoing)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :crew_members
-  resources :productions, only: %i[index show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :crew_members
+  resources :productions, only: %i[index show create update]
 end
