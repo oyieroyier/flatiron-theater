d57cf9334224517f0efa29899f7af4b2bcc8120b
Bob Oyier <boboyier92@gmail.com>
2023-03-15 21:14:18 +0300

    Custom Validations

--- app/models/production.rb
+++ app/models/production.rb
@@ -2,6 +2,15 @@ class Production < ApplicationRecord
   has_many :crew_members
 
   validates :title, presence: true
+  validate :no_more_musicals
+
+  # Custom Validations
+  def no_more_musicals
+    if genre.downcase == 'musical'
+      errors.add(:genre, 'Sorry, no more musicals!')
+    end
+  end
+
   def title_director
     "#{title}: #{director}"
   end
